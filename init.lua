be490d3d623b898969793d3f6670d3608fd88f9d
Utkay Daymaz <utkayar@gmail.com>
2024-11-03 00:43:42 +0300

    Adds default colorscheme kanagawa along with others

--- init.lua
+++ init.lua
@@ -104,6 +104,7 @@ require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
+vim.cmd.colorscheme 'kanagawa'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,17 @@
+return {
+  { 'catppuccin/nvim' },
+  { 'ellisonleao/gruvbox.nvim' },
+  { 'rebelot/kanagawa.nvim' },
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+  },
+  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
+  { 'rose-pine/neovim', name = 'rose-pine' },
+  { 'EdenEast/nightfox.nvim' },
+  {
+    'olimorris/onedarkpro.nvim',
+    priority = 1000, -- Ensure it loads first
+  },
+}
