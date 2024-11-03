10131e98653473d1f21478d09b04c58af49f1b13
Utkay Daymaz <utkayar@gmail.com>
2024-11-03 17:13:24 +0300

    Slightly alters statusline

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,123 +1,48 @@
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = 'VeryLazy',
-  init = function()
-    vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
-      vim.o.statusline = ' '
-    else
-      -- hide the statusline on the starter page
-      vim.o.laststatus = 0
-    end
-  end,
-  opts = function()
-    local icons = {
-      diagnostics = { Error = '', Warn = '', Info = '', Hint = '' },
-      git = { added = '', modified = '', removed = '' },
-    }
-
-    vim.o.laststatus = 3 -- Set to 3 to enable global statusline across all buffers
-
-    local opts = {
+  config = function()
+    require('lualine').setup {
       options = {
+        icons_enabled = true,
         theme = 'auto',
-        globalstatus = true, -- Make the statusline consistent across all windows
-        disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'ministarter' } },
+        component_separators = { left = '\\', right = '/' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        lualine_c = {
-          {
-            function()
-              return vim.fn.getcwd()
-            end,
-            separator = '|',
-          },
-          {
-            'diagnostics',
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
-          },
-          { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
-          { 'filename', path = 1 },
-        },
-        lualine_x = {
-          {
-            function()
-              return require('noice').api.status.command.get()
-            end,
-            cond = function()
-              return package.loaded['noice'] and require('noice').api.status.command.has()
-            end,
-            color = { fg = '#6A9FB5' },
-          },
-          {
-            function()
-              return require('noice').api.status.mode.get()
-            end,
-            cond = function()
-              return package.loaded['noice'] and require('noice').api.status.mode.has()
-            end,
-            color = { fg = '#C678DD' },
-          },
-          {
-            function()
-              return '  ' .. require('dap').status()
-            end,
-            cond = function()
-              return package.loaded['dap'] and require('dap').status() ~= ''
-            end,
-            color = { fg = '#E06C75' },
-          },
-          {
-            function()
-              local lazy_status = require 'lazy.status'
-              return lazy_status.updates()
-            end,
-            cond = function()
-              local lazy_status = require 'lazy.status'
-              return lazy_status.has_updates()
-            end,
-            color = { fg = '#E5C07B' },
-          },
-          {
-            'diff',
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-        },
-        lualine_y = {
-          { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
-          { 'location', padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return ' ' .. os.date '%R'
-          end,
-        },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
       },
-      extensions = { 'neo-tree', 'lazy' },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
     }
-
-    return opts
   end,
 }
