d01e30dcd9081e87631dc810840bda6f4aff9bca
Utkay Daymaz <utkayar@gmail.com>
2024-11-04 00:11:59 +0300

    Adds noice statuses to lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
       options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '\\', right = '/' },
+        component_separators = '',
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
           statusline = {},
@@ -27,7 +27,38 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'filetype' },
+        lualine_x = {
+          {
+            function()
+              return require('noice').api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded['noice'] and require('noice').api.status.command.has()
+            end,
+            color = { fg = '#6A9FB5' },
+          },
+          {
+            function()
+              return require('noice').api.status.mode.get()
+            end,
+            cond = function()
+              return package.loaded['noice'] and require('noice').api.status.mode.has()
+            end,
+            color = { fg = '#C678DD' },
+          },
+          {
+            function()
+              local lazy_status = require 'lazy.status'
+              return lazy_status.updates()
+            end,
+            cond = function()
+              local lazy_status = require 'lazy.status'
+              return lazy_status.has_updates()
+            end,
+            color = { fg = '#E5C07B' },
+          },
+          'filetype',
+        },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
