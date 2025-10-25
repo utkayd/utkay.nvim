dd2110de4636b125182dddcc70cf030182a8aa04
Utkay Daymaz <utkayar@gmail.com>
2025-10-26 00:40:47 +0300

    Updates colorschemes

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -2,7 +2,29 @@ return {
   {
     'catppuccin/nvim',
     priority = 1000,
+    opts = {},
     config = function()
+      require('catppuccin').setup {
+        transparent_background = false, -- Keep this false to preserve colors
+        custom_highlights = function(colors)
+          return {
+            -- Line numbers
+            LineNr = { fg = colors.overlay2 },
+            CursorLineNr = { fg = colors.yellow, style = { 'bold' } },
+
+            -- Make backgrounds transparent (inherit from terminal)
+            Normal = { bg = 'NONE' },
+            NormalNC = { bg = 'NONE' },
+            NormalFloat = { bg = 'NONE' },
+            FloatBorder = { bg = 'NONE' },
+            Pmenu = { bg = 'NONE' },
+            SignColumn = { bg = 'NONE' },
+            -- Uncomment if you want statusline/tabline transparent too:
+            StatusLine = { bg = 'NONE' },
+            TabLine = { bg = 'NONE' },
+          }
+        end,
+      }
       vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
