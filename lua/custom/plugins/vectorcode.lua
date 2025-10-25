a142cf455739f2905c632cba8cf4aa22e1f90d75
Utkay Daymaz <utkayar@gmail.com>
2025-10-25 12:10:11 +0300

    Removes vectorcode

--- lua/custom/plugins/vectorcode.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'Davidyz/VectorCode',
-  version = '0.6.12', -- optional, depending on whether you're on nightly or release
-  build = 'pipx upgrade vectorcode', -- optional but recommended. This keeps your CLI up-to-date.
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  cmd = 'VectorCode',
-}
