30fc701dcfe9f2ae33cc2b1dd2db6457fa2dc51a
Utkay Daymaz <utkayar@gmail.com>
2025-10-26 15:02:51 +0300

    Omits unused colorschemes

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -36,20 +36,4 @@ return {
       vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
-  { 'ellisonleao/gruvbox.nvim' },
-  {
-    'rebelot/kanagawa.nvim',
-    init = function()
-      require('kanagawa').setup {
-        transparent = true,
-      }
-    end,
-  },
-  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
-  { 'rose-pine/neovim', name = 'rose-pine' },
-  { 'EdenEast/nightfox.nvim' },
-  {
-    'olimorris/onedarkpro.nvim',
-    priority = 1000, -- Ensure it loads first
-  },
 }
