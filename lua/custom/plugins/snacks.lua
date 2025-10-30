7c142e45318a9b2d0908b672bc2b7240d823e128
Utkay Daymaz <utkayar@gmail.com>
2025-10-30 20:58:20 +0300

    Updates neovim news border

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -572,6 +572,7 @@ return {
           file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
           width = 0.6,
           height = 0.6,
+          border = 'rounded',
           wo = {
             spell = false,
             wrap = false,
